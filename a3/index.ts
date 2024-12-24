669088fe5218da978f78c8bf7868d95038261dfe
WENDER R DE QUEIROZ <166562387+WenderDevProgramador@users.noreply.github.com>
2024-12-24 16:18:29 -0300

    Atu

---
+++ a3/index.ts
@@ -0,0 +1,21 @@
+function sendSpaceship(spaceship: {pilot: string, copilot?: string}) {
+
+}
+
+sendSpaceship({pilot: 'Wender', copilot: 'Rodolfo'})
+
+sendSpaceship({pilot: 'Zé'})
+
+let input: unknown
+
+input = 'test'
+input = 23
+input = []
+
+let variavel: any
+
+variavel = 'test'
+variavel = 23
+variavel = []
+variavel = {}
+
