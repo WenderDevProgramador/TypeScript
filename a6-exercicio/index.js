dd2a41135b77e6d4c912b82d478f5da03fa9622b
WENDER R DE QUEIROZ <166562387+WenderDevProgramador@users.noreply.github.com>
2024-12-27 14:46:47 -0300

    add

--- a6-exercicio/index.js
+++ a6-exercicio/index.js
@@ -1,1 +1,53 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const users = [];
+function fetchUser(username) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const response = yield fetch(`https://api.github.com/users/${username}`);
+        const user = yield response.json();
+        if (user.message) {
+            alert('Usuário não encontrado!');
+        }
+        else {
+            users.push(user);
+            alert(`O usuário ${user.login} foi salvo.\n` +
+                `\nid: ${user.id}` +
+                `\nlogin: ${user.login}` +
+                `\nNome: ${user.name}` +
+                `\nBio: ${user.bio}` +
+                `\nRepositórios públicos: ${user.public_repos}`);
+        }
+    });
+}
+function showUser(username) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const user = users.find(user => user.login === username);
+        if (typeof user === 'undefined') {
+            alert('Usuario não encontrado!');
+        }
+        else {
+            const response = yield fetch(user.repos_url);
+            const repos = yield response.json();
+            let message = `id: ${user.id}\n` +
+                `\nlogin: ${user.login}` +
+                `\nNome: ${user.name}` +
+                `\nBio: ${user.bio}` +
+                `\nRepositórios públicos: ${user.public_repos}`;
+            repos.forEach(repo => {
+                message += `\nNome: ${repo.name}` +
+                    `\nDescrição: ${repo.description}` +
+                    `\nEstrelas: ${repo.stargazers_count}` +
+                    `\nÉ um fork: ${repo.fork ? 'Sim' : 'Não'}\n`;
+            });
+            alert(message);
+        }
+    });
+}
