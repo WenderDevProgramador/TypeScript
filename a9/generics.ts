ae4478138d802adf2a9b7c3cbeb45164accfe665
WENDER R DE QUEIROZ <166562387+WenderDevProgramador@users.noreply.github.com>
2025-01-02 18:34:56 -0300

    atu

--- a9/generics.ts
+++ a9/generics.ts
@@ -2,6 +2,57 @@ function first(array: any[]) {
     return array[0]
 }
 
-function last<Type>(array: Type[]) {
+function last<Type>(array: Type[]): Type | undefined {
     return array[array.length - 1]
 }
+
+const pilots = ['Luke', 'Biggs', 'Wedge', 'Han', 'Lando']
+
+const firstPilot = first(pilots)
+
+const lastPilots = last(pilots)
+
+interface Ship {
+    name: string
+    pilot: string
+}
+
+interface Fighter extends Ship {
+    weapons: number
+    shields: number
+}
+
+interface Transport extends Ship {
+    capacity: number
+}
+
+interface Speeder extends Ship {
+    speed: number
+    acceleration: number
+}
+
+// Se deixássemos sem o tipo Ship desativaríamos
+// totalmente o typescript para esse argumento
+function cloneShip(ship: Ship, newName: string, newPilot: string) {
+    const newShip = ship
+    newShip.name = newName
+    newShip.pilot = newPilot
+    return newShip
+}
+
+const falcon: Ship = {
+    name: 'Millenium Falcon',
+    pilot: 'Han'
+}
+
+const xWing: Fighter = {
+    name: 'Red Five',
+    pilot: 'Luke',
+    weapons: 4,
+    shields: 1
+}
+
+// A cópia funciona, porém a tipagem está incorreta
+// pois a ambas é atribuido o tipo Ship
+const copy1 = cloneShip(falcon, 'Milano', 'Peter')
+const copy2 = cloneShip(xWing, 'Black One', 'Poe')
