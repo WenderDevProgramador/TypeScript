fc1a2236a7d3e0177ee4f259de304cc7917b2594
WENDER R DE QUEIROZ <166562387+WenderDevProgramador@users.noreply.github.com>
2025-09-27 18:14:07 -0300

    add

---
+++ Revisão/Exercicios/tuplas.ts
@@ -0,0 +1,15 @@
+let crew: string[] = [];
+let avocado: [string,string,string] = ['Wender', 'Rodolfo', 'Queiroz'];
+
+avocado[0] = 'Sileide'
+avocado[1] = 'Freitas'
+avocado[2] = 'Silva'
+
+crew.push('Minha avó tá maluca')
+
+let point: [number, number, string, boolean]
+
+point = [2,5,'p', true]
+
+let [x,y] = point
+
